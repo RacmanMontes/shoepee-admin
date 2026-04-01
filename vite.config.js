9b8254ea9d559ad4c090e70dbcb39b5978cad2d4
RacmanMontes <racmanmontes@gmail.com>
2026-04-01 19:42:44 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/admin/',  // Add this line for deployment
   server: {
     port: 3000,
     open: true,
